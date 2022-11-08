7d8478d06405454ca6d4f54d44a7ba840396892d
dlavinia <dlavinia2003@gmail.com>
2022-11-08 10:33:23 -0300

    Todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './templates/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormComponent } from './template/form/form.component';
+import { FormComponent } from './templates/form/form.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
